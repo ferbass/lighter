73b32753f99a269277f2582af63d73e238504721
Fernando Bass <ferbass@gmail.com>
2011-05-03 22:45:59 -0300

    first commmit

---
+++ cocos2d_particle_example/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  cocos2d_particle_example
+//
+//  Created by Fernando Ribeiro on 03/05/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ cocos2d_particle_example/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  cocos2d_particle_example
+//
+//  Created by Fernando Ribeiro on 03/05/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
